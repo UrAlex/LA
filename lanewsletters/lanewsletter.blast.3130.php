1b93d6ff7bcd7ef3cd7a8a7c3534395b2dbb65b6
UrAlex <ajs@urvenue.com>
2013-07-10 14:19:46 -0500

    Create lanewsletter.blast.3130.php

---
+++ lanewsletters/lanewsletter.blast.3130.php
@@ -0,0 +1,276 @@
+
+  	<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+		<html xmlns='http://www.w3.org/1999/xhtml'>
+			<head>	
+				<title>Los Angeles</title>	
+				<meta name='viewport' content='width=device-width' />
+				<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+				<script type='text/javascript' src='http://urmaster.com/zc8_jquery/jquery.js'></script>
+				<script type='text/javascript'>
+					 window.onload = function() {
+					 	position();
+			    	 }
+		    	 
+			    	 
+					  function position()
+			         {
+			         
+			          var css = document.createElement('style');
+						css.type = 'text/css';
+						
+						document.body.appendChild(css);
+			         
+				         if(window.innerWidth<700){
+				         	css.innerHTML = 'table { width: 650px } .uv_line2{width:34%;} .uv_maintable{width:650px } img {width: 100% } ';
+				         }
+				         else
+				         {
+					     	css.innerHTML = '.uv_maintable { width: 650px } table { width: 650px } img {width: 100% }  .uv_line2{width:34%;}';   
+				         }
+				         document.body.appendChild(css);
+			         }
+					window.onresize=position;
+										
+				</script>
+				<style type='text/css'>
+					@import url(https://fonts.googleapis.com/css?family=Oswald:400,700,300);
+				</style>
+			</head>
+			<body style='background:#000;'>
+			<table style='width:100%;background-color:black'><tr><td>
+			<div style='padding-top:5px;width: 650px;margin: auto;color: white;font-family:Oswald,Sans Serif, Arial;background-color:black;margin:auto'></div>
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		<table align="center" cellpadding="0" cellspacing="0" width="650" class="uv_maintable" style="max-width:650px; display:block;">
+				<tbody>
+				<tr>
+					<td id="customtitle" style="padding-top:25px;"></td>
+				</tr>
+				<!-- top -->
+
+				<tr>
+					<td align="center" width="650">	
+						<a href="http://play.sbe.com/index.cfm?key=O52NWSMKuM&amp;id=1410&amp;searchCity=Los-Angeles&amp;utm_source=Nightlife%2BDatabase&amp;utm_medium=email&amp;utm_campaign=Nightlife%2BWeekly%2BNewsletter%2B02.20.2013" target="_blank">
+							<img src="http://urmaster.com/promoters/imageslosangeles/top1.jpg" width="650" border="0" style="display:block;" alt="sbe">
+						</a>
+					</td>
+				</tr>
+				<tr>
+					<td align="center">
+						<table cellpadding="0" cellspacing="0" style="max-width:650px; display:block; background-image:url('http://urmaster.com/promoters/imageslosangeles/topbg.jpg');background-size:100%;background-color:black">
+						<tbody><tr>
+							<td align="center" width="318">
+								<a href="http://play.sbe.com/index.cfm?key=O52NWSMKuM&amp;id=1410&amp;searchCity=Los-Angeles&amp;utm_source=Nightlife%2BDatabase&amp;utm_medium=email&amp;utm_campaign=Nightlife%2BWeekly%2BNewsletter%2B02.20.2013" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/top2a.jpg" width="318" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="">
+								</a>
+							</td>
+							<td width="26" style="background:#000;">
+								<a href="http://play.sbe.com/index.cfm?key=O52NWSMKuM&amp;id=1410&amp;searchCity=Los-Angeles&amp;utm_source=Nightlife%2BDatabase&amp;utm_medium=email&amp;utm_campaign=Nightlife%2BWeekly%2BNewsletter%2B02.20.2013" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/topblack1.jpg" width="26" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="">
+								</a>
+							</td>
+							<td align="center" width="137">
+								<a href="http://www.sbe.com/plan_your_event.php" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/top2b.jpg" width="137" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="">
+								</a>
+							</td>
+							<td align="center" style="background-color:black" width="18">
+								<a href="http://play.sbe.com/index.cfm?key=O52NWSMKuM&amp;id=1410&amp;searchCity=Los-Angeles&amp;utm_source=Nightlife%2BDatabase&amp;utm_medium=email&amp;utm_campaign=Nightlife%2BWeekly%2BNewsletter%2B02.20.2013" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/topblack2.jpg" width="18" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="">
+								</a>
+							</td>
+							
+							<td align="center" width="32">
+								<a href="http://instagram.com/sbe_nightlife" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/social1.jpg" width="32" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="instagram">
+								</a>
+							</td>
+							
+							<td align="center" width="32">
+								<a href="https://www.facebook.com/EmersonTheatre" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/social2.jpg" width="32" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="facebook">
+								</a>
+							</td>
+							
+							<td align="center" width="32">
+								<a href="https://twitter.com/sbe_Nightlife" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/social3.jpg" width="32" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="twitter">
+								</a>
+							</td>
+							
+							<td align="center" width="32">
+								<a href="http://www.sbe.com" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/social4.jpg" width="32" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="download our app">
+								</a>
+							</td>
+							
+							<td align="center" style="background-color:black" width="21">
+								<a href="http://play.sbe.com/index.cfm?key=O52NWSMKuM&amp;id=1410&amp;searchCity=Los-Angeles&amp;utm_source=Nightlife%2BDatabase&amp;utm_medium=email&amp;utm_campaign=Nightlife%2BWeekly%2BNewsletter%2B02.20.2013" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/black2.jpg" width="21" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="">
+								</a>
+							</td>
+							
+							
+						</tr>
+						</tbody></table>
+					</td>
+				</tr>
+				<tr>
+					<td align="center">
+						<a href="http://play.sbe.com/index.cfm?key=O52NWSMKuM&amp;id=1410&amp;searchCity=Los-Angeles&amp;utm_source=Nightlife%2BDatabase&amp;utm_medium=email&amp;utm_campaign=Nightlife%2BWeekly%2BNewsletter%2B02.20.2013" target="_blank">
+							<img src="http://urmaster.com/promoters/imageslosangeles/top3.jpg" width="650" border="0" style="display:block; height:auto;" alt="">
+						</a>
+					</td>
+				</tr>
+				
+				<!-- end top-->
+				
+				<!-- RELIVE WEEK -->
+				<tr>
+					<td align="center">
+						<table width="650" style="background:#000;">
+							<tbody><tr>
+								<td align="center" width="156">
+										<img src="http://urmaster.com/promoters/imageslosangeles/sbe2-1.jpg" width="100%" border="0" class="" style="display:block" alt="relive last week">
+									
+								</td>
+								<td width="2" align="center">
+									<img src="http://urmaster.com/promoters/imageslosangeles/line.png" border="0" width="2" style="display:block" alt="">
+								</td>
+								
+								<td align="center" width="156">
+									<a id="hrefrlv1" href="https://www.facebook.com/media/set/?set=a.403121006469070.1073741842.370045719776599&amp;type=3" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/sbe2-2.jpg" width="100%" border="0" class="" style="display:block" alt="photos" id="rlv1">
+									</a>
+								</td>
+								
+							
+								<td align="center" width="156">
+									<a id="hrefrlv2" href="http://www.youtube.com/watch?v=jgtNxSwp72s" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/sbe2-3.jpg" width="100%" border="0" class="" style="display:block" alt="video" id="rlv2">
+									</a>
+								</td>
+								
+								<td width="2" align="center">
+									<img src="http://urmaster.com/promoters/imageslosangeles/line.png" border="0" width="2" style="display:block" alt="">
+								</td>
+								
+								<td align="center" width="176">
+									<a id="hrefrlv3" href="http://www.examiner.com/article/the-library-bar-at-the-redbury-the-definition-of-sophistication-and-fun" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/sbe2-4.jpg" width="100%" border="0" class="" style="display:block" alt="press" id="rlv3">
+									</a>
+								</td>
+								
+							</tr>
+						</tbody></table>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+			<!-- END RELIVE WEEK -->
+			
+			<!-- EVENTS SECTION -->
+				<!-- 1 -->
+		<table align="center" cellpadding="0" cellspacing="0" id="showtableevent" width="650" class="uv_maintable" style="max-width:650px; display:block;">			
+				<tbody>				
+
+				<tr evts="" type="evt" id="trevt2" evtid="99453" date="2013-07-10" name="Emerson Wednesday" imgid="img2" target="evt2" style="margin-top:10px"><td align="center"><a id="evt2" type="targetevt" href="http://emersontheatre.com/show.cfm?id=99453&amp;cart&amp;utm_medium=email&amp;utm_campaign=emerson-wednesday-2013-07-10&amp;key=4YzVDEX23G&amp;utm_source=newsletter-Zenon-C"><img id="img2" src="http://urmaster.com/promoters/lanewsletter/lasmartimage.php?img=sbe1.jpg&amp;date=2013-07-10" border="0" style="display:block; height:auto; width:auto" alt="Emerson Wednesday"></a></td></tr><tr evts="" type="evt" id="trevt3" evtid="101069" date="2013-07-12" name="Create Friday" imgid="img3" target="evt3" style="margin-top:10px"><td align="center"><a id="evt3" type="targetevt" href="http://createnightclub.com/show.cfm?id=101069&amp;cart&amp;utm_medium=email&amp;utm_campaign=create-friday-2013-07-12&amp;key=4YzVDEX23G&amp;utm_source=newsletter-Zenon-C"><img id="img3" src="http://urmaster.com/promoters/lanewsletter/lasmartimage.php?img=sbe2.jpg&amp;date=2013-07-12" border="0" style="display:block; height:auto; width:auto" alt="Create Friday"></a></td></tr><tr evts="" type="evt" id="trevt4" evtid="101070" date="2013-07-13" name="Create Saturday" imgid="img4" target="evt4" style="margin-top:10px"><td align="center"><a id="evt4" type="targetevt" href="http://createnightclub.com/show.cfm?id=101070&amp;cart&amp;utm_medium=email&amp;utm_campaign=create-saturday-2013-07-13&amp;key=4YzVDEX23G&amp;utm_source=newsletter-Zenon-C"><img id="img4" src="http://urmaster.com/promoters/lanewsletter/lasmartimage.php?img=sbe3.jpg&amp;date=2013-07-13" border="0" style="display:block; height:auto; width:auto" alt="Create Saturday"></a></td></tr><tr evts="" type="evt" id="trevt5" evtid="99243" date="2013-07-13" name="Greystone Saturday" imgid="img5" target="evt5" style="margin-top:10px"><td align="center"><a id="evt5" type="targetevt" href="http://greystonemanorla.com/show.cfm?id=99243&amp;cart&amp;utm_medium=email&amp;utm_campaign=greystone-saturday-2013-07-13&amp;key=4YzVDEX23G&amp;utm_source=newsletter-Zenon-C"><img id="img5" src="http://urmaster.com/promoters/lanewsletter/lasmartimage.php?img=sbe4.jpg&amp;date=2013-07-13" border="0" style="display:block; height:auto; width:auto" alt="Greystone Saturday"></a></td></tr><tr evts="" type="evt" id="trevt6" evtid="102056" date="2013-07-13" name="Eden Saturday" imgid="img6" target="evt6" style="margin-top:10px"><td align="center"><a id="evt6" type="targetevt" href="http://edenhollywood.com/show.cfm?id=102056&amp;cart&amp;utm_medium=email&amp;utm_campaign=eden-saturday-2013-07-13&amp;key=4YzVDEX23G&amp;utm_source=newsletter-Zenon-C"><img id="img6" src="http://urmaster.com/promoters/lanewsletter/lasmartimage.php?img=sbe5.jpg&amp;date=2013-07-13" border="0" style="display:block; height:auto; width:auto" alt="Eden Saturday"></a></td></tr><tr evts="" type="evt" id="trevt7" evtid="101063" date="2013-07-14" name="XIV Sunday" imgid="img7" target="evt7" style="margin-top:10px"><td align="center"><a id="evt7" type="targetevt" href="http://xivsummersessions.com/show.cfm?id=101063&amp;cart&amp;utm_medium=email&amp;utm_campaign=xiv-sunday-2013-07-14&amp;key=4YzVDEX23G&amp;utm_source=newsletter-Zenon-C"><img id="img7" src="http://urmaster.com/promoters/lanewsletter/lasmartimage.php?img=sbe6.jpg&amp;date=2013-07-14" border="0" style="display:block; height:auto; width:auto" alt="XIV Sunday"></a></td></tr><tr evts="" type="evt" id="trevt8" evtid="1" date="2013-07-16" name="Sayers club" imgid="img8" target="evt8" style="margin-top:10px"><td align="center"><a id="evt8" type="targetevt" href="http://sayersclub.com/?&amp;cart&amp;utm_medium=email&amp;utm_campaign=sayers-club-2013-07-16&amp;key=4YzVDEX23G&amp;utm_source=newsletter-Zenon-C"><img id="img8" src="http://urmaster.com/promoters/lanewsletter/lasmartimage.php?img=sbe7.jpg&amp;date=2013-07-16" border="0" style="display:block; height:auto; width:auto" alt="Sayers club"></a></td></tr></tbody></table>			
+		<!-- END EVENT SECTION -->	
+				<!-- links -->		
+		<table align="center" cellpadding="0" cellspacing="0" width="650" class="uv_maintable" style="max-width:650px; display:block;">
+		
+				<tbody><tr>
+					<td align="center">
+					<table style="background-color:black" cellspacing="0" cellpadding="0" width="650">
+						<tbody><tr>	
+							<td align="center" width="299">
+								<a href="http://play.sbe.com/index.cfm?key=O52NWSMKuM&amp;id=1410&amp;searchCity=Miami&amp;utm_source=Nightlife%2BDatabase&amp;utm_medium=email&amp;utm_campaign=Nightlife%2BWeekly%2BNewsletter%2B02.20.2013" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/miami.jpg" width="100%" border="0" class="" style="display:block; height:auto;" alt="miami">
+								</a>
+							</td>
+							<td align="center" width="351">
+								<a href="http://play.sbe.com/index.cfm?key=O52NWSMKuM&amp;id=1410&amp;searchCity=Las-Vegas&amp;utm_source=Nightlife%2BDatabase&amp;utm_medium=email&amp;utm_campaign=Nightlife%2BWeekly%2BNewsletter%2B02.20.2013" target="_blank">
+									<img src="http://urmaster.com/promoters/imageslosangeles/lasvegas.jpg" width="100%" border="0" class="" style="display:block; height:auto;" alt="las vegas">
+								</a>
+							</td>
+						</tr>
+					</tbody></table>
+					</td>
+				</tr>
+				
+				<!-- end links-->
+				
+				<!--footer -->
+				<tr>
+					<td align="center">
+						<table cellpadding="0" cellspacing="0" align="center" width="650" style="background-color:black">
+							<tbody><tr>
+								<td align="center" width="438">
+									<a href="http://www.sbe.com/" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/footerblack1.jpg" width="438" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="">
+									</a>
+								</td>
+								
+								<td align="center" width="48">
+									<a href="http://instagram.com/sbe_nightlife" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/footersocial1.jpg" width="48" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="instagram">
+									</a>
+								</td>
+								
+								<td align="center" width="48">
+									<a href="https://www.facebook.com/EmersonTheatre" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/footersocial2.jpg" width="48" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="facebook">
+									</a>
+								</td>
+								
+								<td align="center" width="48">
+									<a href="https://twitter.com/sbe_Nightlife" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/footersocial3.jpg" width="48" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="twitter">
+									</a>
+								</td>
+								
+								<td align="center" width="48">
+									<a href="http://www.sbe.com" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/footersocial4.jpg" width="48" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="sbe">
+									</a>
+								</td>
+								
+								<td align="center" style="background-color:black" width="20">
+									<a href="http://www.sbe.com/" target="_blank">
+										<img src="http://urmaster.com/promoters/imageslosangeles/footerblack2.jpg" width="20" border="0" class="uv_imgmiddle" height="100%" style="display:block; height:auto;" alt="">
+									</a>
+								</td>
+							</tr>
+						</tbody></table>
+					</td>
+				</tr>
+				
+				<tr>
+					<td align="center">
+						<a href="http://www.sbe.com/" target="_blank">
+							<img src="http://urmaster.com/promoters/imageslosangeles/footer.jpg" width="650" border="0" style="display:block; height:auto;" alt="">
+						</a>
+					</td>
+				</tr>
+				
+				<!--end footer-->
+			</tbody></table>																																																																																								
+		</td></table>
+			</body>			
+			
+		</html>		
